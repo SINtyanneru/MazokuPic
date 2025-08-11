94143113a705ff105cb1822f74c04e5253f83eea
SINtyanneru <irjitgd@gmail.com>
2025-08-11 18:59:36 +0900

    うい

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ BUILD_DIR="build"
 echo "Building..."
 echo "Date $DATE"
 
+mkdir -p $BUILD_DIR
+mkdir -p $BUILD_DIR/cd_root
+
 #一旦クリーンアップ
 rm $BUILD_DIR/*.cdi
 rm $BUILD_DIR/cd_root/*.BIN
@@ -24,5 +27,5 @@ elf2bin $BUILD_DIR/$NAME.elf
 scramble $BUILD_DIR/$NAME.bin $BUILD_DIR/cd_root/1ST_READ.BIN
 
 #IPを作成して、cdiファイルを生成
-makeip -l iplogo.png ip.txt -v $BUILD_DIR/IP.BIN -f
+makeip -v -f -l iplogo.png ip.txt build/IP.BIN
 makedisc $BUILD_DIR/$DATE.cdi $BUILD_DIR/cd_root $BUILD_DIR/IP.BIN Rumisan
